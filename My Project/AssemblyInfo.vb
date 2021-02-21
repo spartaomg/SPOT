fce41fb886213c0d3a34ec0133d5618c85339f4e
Sparta <spartaofomg@gmail.com>
2021-02-21 00:26:29 -0500

    210221 - New build using .NET Framework 4.5

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.21.0220")>
-<Assembly: AssemblyFileVersion("1.0.21.0220")>
+<Assembly: AssemblyVersion("1.0.21.0221")>
+<Assembly: AssemblyFileVersion("1.0.21.0221")>
